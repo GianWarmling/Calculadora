f25c8fa08792271d0d0e66ea2d501f9d3d69105d
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2025-05-08 20:56:03 -0300

    Subindo Inicio Projeto Calculadora

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        enum Menu { Soma, Subtração, Divisão, Multiplicação, Potência, Raiz, Sair }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja Bem Vindo a Calculadora, selecione uma das opções:");
+            Console.WriteLine("1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n5-Potência\n6-Raiz\n7-Sair");
+
+            Console.ReadLine();
+        }
+    }
+}
