66657a84eadfc6582397419e115b95adc4209ba4
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2025-05-08 21:08:58 -0300

    Subindo Projeto Calculadora

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -8,12 +8,16 @@ namespace Calculadora
 {
     internal class Program
     {
-        enum Menu { Soma, Subtração, Divisão, Multiplicação, Potência, Raiz, Sair }
+        enum Menu { Soma = 1, Subtração = 2, Divisão = 3, Multiplicação = 4, Potência = 5, Raiz = 6, Sair = 7 }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Seja Bem Vindo a Calculadora, selecione uma das opções:");
             Console.WriteLine("1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n5-Potência\n6-Raiz\n7-Sair");
+
+            Menu opcao = (Menu)int.Parse(Console.ReadLine());
+
+            Console.WriteLine(opcao);
 
             Console.ReadLine();
         }
